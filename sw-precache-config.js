810fa7084918dec4ca35c916863bfbfa2e7dc1af
Junji Shiokawa <junji.shiokawa@fork.co.jp>
2017-08-28 18:48:08 +0900

    new sample-2

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,6 @@ module.exports = {
     staticFileGlobs: [
         '/index.html',
         '/manifest.json',
-        '/pages/*.md',
         '/bower_components/webcomponentsjs/*.js'
     ],
     navigateFallback: 'index.html'
